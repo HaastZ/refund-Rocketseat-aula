3077ff861a52c9ecc5a4e0689041b472126c5d21
Vinicius Almeida <vinicius.vinibola@gmail.com>
2024-10-17 14:24:16 -0300

    crriando uma função para formatar o valor no padrão brasileiro

--- scripts.js
+++ scripts.js
@@ -2,7 +2,22 @@
 const amount = document.getElementById("amount")
 
 amount.oninput = () => {
+    // Obtém o valor do input e remove os caracteres não numéricos
     let value = amount.value.replace(/\D/g, "")
 
-    amount.value = value
+    // Transforma o valor em centavos( exemplo: 150 / 100 = 1.5 que é equivalente a R$ 1,50)
+    value = Number(value) / 100
+
+    // Atualiza o valor do input
+    amount.value = formatCurrencyBRL(value)
+}
+
+function formatCurrencyBRL(value) {
+    // Formata o valor no padrão BRL (Real Brasileiro)
+    value = value.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    })
+
+    return value
 }
